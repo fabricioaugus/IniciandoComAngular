0f047ecd45a57da16df07bcdf54056c4103e8178
fabricioaugus <49557503+fabricioaugus@users.noreply.github.com>
2022-04-01 15:43:32 -0300

    ..

--- src/app/cursos/cursos.service.spec.ts
+++ src/app/cursos/cursos.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { CursosServiceService } from './cursos-service.service';
+import { CursosService } from './cursos.service';
 
-describe('CursosServiceService', () => {
-  let service: CursosServiceService;
+describe('CursosService', () => {
+  let service: CursosService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(CursosServiceService);
+    service = TestBed.inject(CursosService);
   });
 
   it('should be created', () => {
