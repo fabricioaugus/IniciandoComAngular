815c46a6e6c08cad2df88a4ac6401ed18f8d7136
fabricioaugus <49557503+fabricioaugus@users.noreply.github.com>
2022-04-01 14:54:01 -0300

    alteracoes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CursosModule } from './cursos/cursos.module';
+import { PrimeiroComponentComponent } from './primeiro-component/primeiro-component.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponentComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/primeiro-component/primeiro-component.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-primeiro-component',
+  templateUrl: './primeiro-component.component.html',
+  styleUrls: ['./primeiro-component.component.css']
+})
+export class PrimeiroComponentComponent {
+
+}
